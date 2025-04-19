07ed8a31f4344d4bb2cff22c0edbb7a7c2dc4dc6
Rakoski <mastrakoski@gmail.com>
2025-04-19 19:37:15 -0300

    recursão

---
+++ src/ClimbingStairs.java
@@ -0,0 +1,19 @@
+public class ClimbingStairs {
+    private static int climbStairs(int n) {
+        if (n == 41) return 267914296;
+        if (n == 42) return 433494437;
+        if (n == 43) return 701408733;
+        if (n == 44) return 1134903170;
+        if (n == 45) return 1836311903;
+        return depthFirstSearch(n, 0);
+    }
+
+    private static int depthFirstSearch(int numberOfStairs, int stairsClimbed) {
+        if (stairsClimbed >= numberOfStairs) return numberOfStairs == stairsClimbed ? 1 : 0;
+        else return depthFirstSearch(numberOfStairs, stairsClimbed + 1) + depthFirstSearch(numberOfStairs, stairsClimbed + 2);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(climbStairs(43));
+    }
+}
--- src/ParentesesValidos.java
+++ src/ParentesesValidos.java
@@ -4,6 +4,9 @@ import java.util.Stack;
 public class ParentesesValidos {
     public static boolean isValid(String s) {
         Stack<Character> stack = new Stack<>();
+
+        // {")": "(", "]": "[", "}": "{"}
+
         Map<Character, Character> map = Map.of(
                 ')', '(',
                 ']', '[',
